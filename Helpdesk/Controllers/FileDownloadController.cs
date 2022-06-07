784f8c7258c0e28b1850a54963dd33458c1415d0
Warren Garabrandt <wgarabrandt@mn-e.com>
2022-06-07 15:31:47 -0500

    Add DI Base to FileDownload controller

--- Helpdesk/Controllers/FileDownloadController.cs
+++ Helpdesk/Controllers/FileDownloadController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Helpdesk.Data;
+using Helpdesk.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Helpdesk.Controllers
 {
-    public class FileDownloadController : Controller
+    public class FileDownloadController : DI_BaseController
     {
-        public IActionResult Index()
+
+        public FileDownloadController(ApplicationDbContext dbContext,
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+            : base(dbContext, userManager, signInManager)
+        { }
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            await LoadControllerSettings(ViewData);
+            if (_currentHelpdeskUser == null)
+            {
+                // This happens when a user logs in, but hasn't set up their profile yet.
+                return Forbid();
+                // For some pages, it might make sense to redirect to the account profile page so they can immediately enter their details.
+                //return RedirectToPage("/Identity/Account/Manage");
+            }
+            throw new NotImplementedException("File download checks, and return File()");
+            return Forbid();
         }
     }
 }
