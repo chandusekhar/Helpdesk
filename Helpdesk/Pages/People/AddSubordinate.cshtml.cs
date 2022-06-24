414d179c2833ebeff8cc8a7fc6f2d50e9714c67d
wmgarab@hotmail.com <wmgarab@hotmail.com>
2022-06-24 14:04:12 -0500

    Add subordinate/supervisor pages, but not implemented yet.

--- Helpdesk/Pages/People/AddSubordinate.cshtml.cs
+++ Helpdesk/Pages/People/AddSubordinate.cshtml.cs
@@ -1,12 +1,39 @@
+using Helpdesk.Authorization;
+using Helpdesk.Data;
+using Helpdesk.Infrastructure;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Helpdesk.Pages.People
 {
-    public class AddSubordinateModel : PageModel
+    public class AddSubordinateModel : DI_BasePageModel
     {
-        public void OnGet()
+        public AddSubordinateModel(ApplicationDbContext dbContext,
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+            : base(dbContext, userManager, signInManager)
+        { }
+        public async Task<IActionResult> OnGetAsync(string id)
         {
+            await LoadSiteSettings(ViewData);
+            if (_currentHelpdeskUser == null)
+            {
+                // This happens when a user logs in, but hasn't set up their profile yet.
+                return Forbid();
+                // For some pages, it might make sense to redirect to the account profile page so they can immediately enter their details.
+                //return RedirectToPage("/Identity/Account/Manage");
+            }
+            bool HasClaim = await RightsManagement.UserHasClaim(_context, _currentHelpdeskUser.IdentityUserId, ClaimConstantStrings.UsersAdmin);
+            if (!HasClaim)
+            {
+                return Forbid();
+            }
+            if (id == null || _context.HelpdeskUsers == null || _userManager == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
